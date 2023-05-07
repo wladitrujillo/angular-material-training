c9b901931faea52096fc4c80dbbf8b0429ada3b0
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2023-05-07 17:01:32 -0500

    feat: mat-datepicker

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
-import {UntypedFormBuilder, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import { UntypedFormBuilder, Validators } from '@angular/forms';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
 
 @Component({
   selector: "create-course-step-1",
-  templateUrl:"create-course-step-1.component.html",
+  templateUrl: "create-course-step-1.component.html",
   styleUrls: ["create-course-step-1.component.scss"]
 })
 export class CreateCourseStep1Component {
@@ -15,12 +16,21 @@ export class CreateCourseStep1Component {
       Validators.minLength(5),
       Validators.maxLength(60)
     ]],
-    releasedAt: [new Date(), Validators.required],
+    releasedAt: [new Date(1990,0,16), Validators.required],
     category: ['BEGINNER', Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
     longDescription: ['', [Validators.required, Validators.minLength(3)]]
   });
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    const date = cellDate.getDate();
+    if (view == 'month') {
+      return (date == 1) ? 'highlight-date' : '';
+    }
+
+    return '';
+  }
 
   constructor(private fb: UntypedFormBuilder) {
 
