dbc676571a642793710ea34d7517bb68c8cf1a11
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2023-05-09 23:04:39 -0500

    feat: add mat-paginator

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -23,6 +23,9 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   loading: boolean = false;
 
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
+
   constructor(private route: ActivatedRoute,
     private coursesService: CoursesService) {
 
@@ -41,7 +44,10 @@ export class CourseComponent implements OnInit, AfterViewInit {
   loadLessonsPage() {
     this.loading = true;
     this.coursesService
-      .findLessons(this.course.id, "asc", 0, 3)
+      .findLessons(
+        this.course.id, "asc",
+        this.paginator?.pageIndex ?? 0,
+        this.paginator?.pageSize ?? 3)
       .pipe(
         tap(lessons => this.lessons = lessons),
         catchError(err => {
@@ -55,7 +61,11 @@ export class CourseComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-
+    this.paginator.page
+      .pipe(
+        tap(() => this.loadLessonsPage())
+      )
+      .subscribe()
 
   }
 
